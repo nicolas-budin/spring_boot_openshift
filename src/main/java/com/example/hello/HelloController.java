eb98899fb9143338453fa36abb83421a2fc0af6f
nicolas-budin <np.budin@gmail.com>
2026-09-06 20:16:27 +0200

    added config map

--- src/main/java/com/example/hello/HelloController.java
+++ src/main/java/com/example/hello/HelloController.java
@@ -1,5 +1,6 @@
 package com.example.hello;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,15 +12,18 @@ import java.util.Map;
 @RestController
 public class HelloController {
 
+    @Value("${app.message}")
+    private String message;
+
     @GetMapping("/")
     public String hello() {
-        return "Hello World depuis Spring Boot sur OpenShift !";
+        return message;
     }
 
     @GetMapping("/api/hello")
     public Map<String, Object> helloApi() {
         Map<String, Object> body = new LinkedHashMap<>();
-        body.put("message", "Hello World");
+        body.put("message", message);
         body.put("hostname", hostname());
         return body;
     }
